f2a1db1a2411f5ef73a48db029afe299eafd0274
Timothy Tuen <timothytuen45@gmail.com>
2023-10-04 00:58:17 -0400

    testing updated metaphor

--- server/server.js
+++ server/server.js
@@ -4,13 +4,13 @@ import bodyParser from "body-parser";
 import cors from "cors";
 // import OpenAI from "openai";
 import axios from 'axios';
-// import Metaphor from 'metaphor-node';
-// const metaphor = new Metaphor(process.env.METAPHOR_API_KEY);
+import Metaphor from 'metaphor-node';
+const metaphor = new Metaphor(process.env.METAPHOR_API_KEY);
 
 const app = express()
 // const openai = new OpenAI()
 const corsOptions = { origin: `http://localhost:3000`, credentials: true};
-console.log(process.env.METAPHOR_API_KEY)
+// console.log(process.env.METAPHOR_API_KEY)
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -25,7 +25,6 @@ app.post('/api/generate', async (req, res) => {
     const calPref = req.body.caloriePref;
     const carbPref = req.body.carbPref;
     let USER_INPUT;
-    let response;
     if(!useProtein) {
         if(meats.length == 1) {
             USER_INPUT = `Suggest a list of dishes that uses ${meats[0]}. Preferences: protein = ${proteinPref}, calories=${calPref}, carbohydrates=${carbPref}`
@@ -36,24 +35,24 @@ app.post('/api/generate', async (req, res) => {
         USER_INPUT = `Suggest a list of dishes that uses no meat. Preferences: protein = ${proteinPref}, calories=${calPref}, carbohydrates=${carbPref}`
     }
 
-    await axios.post('https://api.metaphor.systems/search', {
-        query: USER_INPUT,
-        numResults: 5,
-        useAutoprompt: true,
-        type: "neural"
-    }, {
-        headers: {
-            'x-api-key': process.env.METAPHOR_API_KEY,
-            'Content-Type': 'application/json'
-        }
-    }).then(result => {
-        response = result.data;
-        console.log(result.data.results)
-        res.send(result.data.results)
-    }).catch(err => {
-        console.log("error" + err)
-        res.send(err);
-    });
+    // await axios.post('https://api.metaphor.systems/search', {
+    //     query: USER_INPUT,
+    //     numResults: 5,
+    //     useAutoprompt: true,
+    //     type: "neural"
+    // }, {
+    //     headers: {
+    //         'x-api-key': process.env.METAPHOR_API_KEY,
+    //         'Content-Type': 'application/json'
+    //     }
+    // }).then(result => {
+    //     response = result.data;
+    //     console.log(result.data.results)
+    //     res.send(result.data.results)
+    // }).catch(err => {
+    //     console.log("error" + err)
+    //     res.send(err);
+    // });
 
     // const SYSTEM_ROLE = 'You are a chef that has experience with cooking for athletes. Only generate one search query.'
 
@@ -62,9 +61,16 @@ app.post('/api/generate', async (req, res) => {
     //     messages: [{"role": "system", "content": SYSTEM_ROLE},
     //         {"role": "user", "content": USER_INPUT}]
     // });
-    // search_response = metaphor.search(
-    //     query, use_autoprompt=True, start_published_date="2023-06-01"
-    // )
+    await metaphor.search(USER_INPUT, {
+        numResults: 5,
+        startPublishedDate: '2023-01-01'
+    }).then(data => {
+        console.log(data)
+        data.data.results;
+    }).catch(err => {
+        console.log(err);
+        res.send(err);
+    })
 
 })
 
